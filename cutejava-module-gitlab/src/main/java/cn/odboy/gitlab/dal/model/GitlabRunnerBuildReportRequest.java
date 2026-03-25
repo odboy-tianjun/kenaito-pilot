ae037ce1b0ebb008f228bd614bacfabc10260418
Odboy <tianjun@odboy.cn>
2026-03-25 19:54:18 +0800

    feat(gitlab): 添加Gitlab Runner构建上报功能并清理CI模板
    - 新增GitlabRunnerBuildReportRequest模型类，封装构建上报请求参数
    - 新增OpenGitlabWebhooksController控制器，提供免签构建上报接口
    - 日志配置修改为KenaitoPilot，调整日志文件路径和名称
    - 删除多个无用的CI构建模板文件，简化配置管理

---
+++ cutejava-module-gitlab/src/main/java/cn/odboy/gitlab/dal/model/GitlabRunnerBuildReportRequest.java
@@ -0,0 +1,50 @@
+package cn.odboy.gitlab.dal.model;
+
+import cn.odboy.base.KitObject;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Gitlab Runner 流水线构建上报
+ */
+@Getter
+@Setter
+public class GitlabRunnerBuildReportRequest extends KitObject {
+
+  /**
+   * 项目id
+   */
+  private String projectId;
+  /**
+   * 项目提交的完整SHA哈希值
+   */
+  private String commitId;
+  /**
+   * 流水线id
+   */
+  private String pipelineId;
+  /**
+   * 任务id
+   */
+  private String jobId;
+  /**
+   * 上下文名称
+   */
+  private String contextName;
+  /**
+   * 版本号
+   */
+  private String versionCode;
+  /**
+   * 环境编码
+   */
+  private String envCode;
+  /**
+   * 镜像名称（仅当status返回success是，这个字段是有效的）
+   */
+  private String imageName;
+  /**
+   * 状态： success：构建成功 failed：构建失败 canceled：任务被取消 skipped：任务被跳过 manual：手动执行
+   */
+  private String status;
+}

1f833b899f93f2bd83ceaecb8dd614cc966bc8e6
Odboy <tianjun@odboy.cn>
2026-03-28 09:11:43 +0800

    feat(kubernetes): 添加集群客户端和命名空间查询方法
    - 添加根据集群编码获取客户端的方法
    - 添加根据命名空间名称查询的方法
    - 补充相关方法的文档注释

--- cutejava-module-pilot/src/main/java/cn/odboy/kubernetes/repository/KubernetesRepository.java
+++ cutejava-module-pilot/src/main/java/cn/odboy/kubernetes/repository/KubernetesRepository.java
@@ -5,7 +5,20 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 
 public interface KubernetesRepository {
 
+  /**
+   * 根据集群编码获取客户端
+   *
+   * @param clusterCode 集群编码
+   * @return /
+   */
   KubernetesClient getClusterClient(String clusterCode);
 
+  /**
+   * 根据命名空间名称查询
+   *
+   * @param clusterClient /
+   * @param name          命名空间名称
+   * @return /
+   */
   Namespace getNamespaceByName(KubernetesClient clusterClient, String name);
 }
